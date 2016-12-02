35e7216e8c440ab45ac52a76912b9c8ea7ba96cb
harrytsui <harry.tsui@outlook.com>
2016-12-02 13:52:36 +0800

    First commit SocketCom

---
+++ src/TestMain.java
@@ -0,0 +1,15 @@
+
+public class TestMain {
+	
+	public static int SOKCET_PORT = 13267;
+	public static String SERVER = null;
+	// file path
+	public static String FILE_TO_RECEIVED = "";
+	// file size should be bigger than the file to be downloaded
+	public static final int FILE_SIZE = 40000;
+	
+	public static void main(String[] args) {
+		System.out.print("1");
+	}
+
+}
